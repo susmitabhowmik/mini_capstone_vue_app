7f6bc094d483d5814dab7016b0dd66f323edc1df
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-04-04 20:02:43 -0400

    add new action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
-    <h1>products: </h1>
+    <h1> {{message}} </h1>
+    <p>Name:<input type="text" v-model="newProductTitle">
+    <p>Description:<input type="text" v-model="newProductDescription">
+    <p>Price:<input type="text" v-model="newProductPrice">
+     <p>Image Url:<input type="text" v-model="newProductImageUrl">
+
+    <button v-on:click="addNewProduct()">Add a new product</button>
+
+
     <div v-for="product in products">
     <h4>{{product.name }}</h4>
     <p>{{product.description }}</p>
@@ -22,8 +30,12 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
-      products: []
+      message: "Products",
+      products: [],
+      newProductPrice: "",
+      newProductDescription: "",
+      newProductTitle: "",
+      newProductImageUrl: ""
     };
   },
   created: function() {
@@ -35,9 +47,28 @@ export default {
     axios.get("/api/products").then(response => {
 
       this.products = response.data;
-      // console.log(response.data);
+      console.log(response.data);
     });
   },
-  methods: {}
+  methods: {
+    addNewProduct: function() {
+      console.log('adding a new product');
+      //Make a request to the api to the create action
+      // response = HTTP.get("/api/products")
+      // products = response.parse
+      var params = {
+        name: this.newProductTitle,
+        description: this.newProductDescription,
+        price: this.newProductPrice,
+        image_url: this.newProductImageUrl
+
+      }; //sent as body/form params 
+
+      axios.post("/api/products", params).then(response=>{
+        console.log(response);
+        this.products.push(response.data);
+      });
+    }
+  }
 };
 </script>
