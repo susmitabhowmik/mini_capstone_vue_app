58824a9b267945396deb481a967d40618ea562ae
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-04-08 20:04:35 -0400

    add test page

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Test from './views/Test.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -14,6 +15,11 @@ export default new Router({
       component: Home
     },
     {
+      path: '/test',
+      name: 'test',
+      component: Test
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -22,4 +28,4 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
-})
+});
